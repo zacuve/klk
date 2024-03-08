418c6129df88bb9ce63cac69a766243015065018
zacuve <42791044+zacuve@users.noreply.github.com>
2024-03-07 23:00:05 -0800

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,9 @@
+package com.branchapp.encryption.impl;
+
+/* loaded from: classes13.dex */
+public final class BuildConfig {
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final String HEK = "a4mX2eXAxediOauzdKOZVkysMp7MN5vh";
+    public static final String LIBRARY_PACKAGE_NAME = "com.branchapp.encryption.impl";
+}
